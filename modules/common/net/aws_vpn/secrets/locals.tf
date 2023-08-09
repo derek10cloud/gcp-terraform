4646a3de351f80565ff6000cf2f8071a51d05028
derek10cloud <derek@daangn.com>
2023-08-09 11:45:25 +0900

    Add network resource

--- modules/common/net/aws_vpn/secrets/locals.tf
+++ modules/common/net/aws_vpn/secrets/locals.tf
@@ -3,3 +3,8 @@ locals {
   region  = var.region
   # peer_vpn_gw_names = toset([for v in range(var.number_of_peer_vpn_gw_ips) : "${var.peer_vpn_gw_ip_name}-${v + 1}"])
 }
+locals {
+  project = var.project
+  region  = var.region
+  # peer_vpn_gw_names = toset([for v in range(var.number_of_peer_vpn_gw_ips) : "${var.peer_vpn_gw_ip_name}-${v + 1}"])
+}
